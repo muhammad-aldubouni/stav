37b788fb2555c704276a4ac338f8d22a1c8756ce
muhammad-aldubouni <muhammad.aldubouni.expserv@gmail.com>
2025-06-04 18:32:25 +0300

    added a  paramater name to the   animation function

--- lib/src/animations.dart
+++ lib/src/animations.dart
@@ -4,8 +4,8 @@ import 'package:flutter_animate/flutter_animate.dart';
 extension ConditionalAnimation on Animate {
   Animate animateWhere({
     required bool condition,
-    required Animate Function(Animate) animate,
+    required Animate Function(Animate animate) animation,
   }) {
-    return condition ? animate(this) : this;
+    return condition ? animation(this) : this;
   }
 }
