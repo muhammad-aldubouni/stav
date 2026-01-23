63d28da1d1ecb686049b664c4e1e9eecd94fbed2
muhammad-aldubouni <muhammad.aldubouni.expserv@gmail.com>
2026-01-23 19:44:43 +0300

    putting dispose and cleaning logic after the navigation

--- lib/src/service_management.dart
+++ lib/src/service_management.dart
@@ -59,8 +59,8 @@ abstract class BaseViewModel<T extends BaseViewModel<T>> {
       navigate(ctx);
       return;
     }
-    ServiceContainer.registerViewModel<T>(newInstance);
-    dispose();
     navigate(ctx);
+    dispose();
+    ServiceContainer.registerViewModel<T>(newInstance);
   }
 }
