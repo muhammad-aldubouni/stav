7991213441d7862c815d3eaa8ab965849cc42c89
muhammad-aldubouni <muhammad.aldubouni.expserv@gmail.com>
2026-06-01 04:34:16 +0300

    decoupling service management to another pavkage

--- lib/stav.dart
+++ lib/stav.dart
@@ -1,6 +1,5 @@
 export 'package:stav/src/app_shell.dart';
 export 'package:stav/src/state_management.dart';
-export 'package:stav/src/service_management.dart';
 export 'package:stav/src/themes.dart';
 export 'package:stav/src/animations.dart';
 export 'package:go_router/go_router.dart';
